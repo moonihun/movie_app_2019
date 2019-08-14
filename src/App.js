f2378ee79a31345b346c2857aafa6112b2e502f1
문지훈 <moonjihun@moonjihun-ui-MacBookPro.local>
2019-08-14 17:03:28 +0900

    Install prop-types.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,64 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>;
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:
+      "http://recipe1.ezmember.co.kr/cache/recipe/2015/04/02/238f0a3e8b567db1229b0f27220033341.jpg",
+    rating: 5
+  },
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image:
+      "http://image.chosun.com/sitedata/image/201902/24/2019022401106_0.jpg",
+    rating: 4.9
+  },
+  {
+    id: 3,
+    name: "Bibimbap",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/220px-Dolsot-bibimbap.jpg",
+    rating: 4.5
+  },
+  {
+    id: 4,
+    name: "Doncasu",
+    image:
+      "http://homecuisine.co.kr/files/attach/images/140/039/031/d69c05ead7a6eb117d90d12e12fd302a.JPG",
+    rating: 4.8
+  }
+];
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h2>I Like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
 }
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food fav="Kimchi" />
-      <Food fav="Ramen" />
-      <Food fav="Samgiopsal" />
-      <Food fav="Chukumi" />
+      {foodILike.map(dish => (
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
+      ))}
     </div>
   );
 }
